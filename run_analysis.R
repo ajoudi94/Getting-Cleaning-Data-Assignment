e03efc3d1a05bc2a8910b902051d82507322a096
ajoudi94 <ajoudi@live.com>
2020-05-09 03:42:37 -0400

    Push initial script

---
+++ run_analysis.R
@@ -0,0 +1,23 @@
+# Data Collection Assignment Week 4
+
+rm(list = ls())
+library(tidyr)
+
+
+
+
+zip_url <-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+raw_csv_name <- "rawdata.zip"
+raw_link <- paste0(getwd(),"/",raw_csv_name)
+
+download.file(zip_url,raw_link)
+s
+unzip(raw_link)
+
+#You should create one R script called run_analysis.R that does the following.
+
+#1# Merges the training and the test sets to create one data set.
+#2# Extracts only the measurements on the mean and standard deviation for each measurement.
+#3# Uses descriptive activity names to name the activities in the data set
+#4# Appropriately labels the data set with descriptive variable names.
+#5# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
